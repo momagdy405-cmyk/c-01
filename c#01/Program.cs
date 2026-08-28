c6c73ac0d90f770208a7dd4feaff9a8a75fd49bb
momagdy405-cmyk <momagdy405@gmail.com>
2026-08-28 22:00:39 +0300

    Add project files.

---
+++ c#01/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Dynamic;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace c_01
+{
+    #region q1,q2
+
+
+    /*
+    class Book
+    {
+        public string title = default!;
+        public int pages;
+    }
+    */
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1,Q2
+            //Create a Book class with a Title(string) and Pages(int). Create a Book object and store it in a variable of type object.Print it.
+
+            //Using the Book class above, print the result of calling ToString(), Equals() (compare book with itself), GetHashCode(), and GetType() on book.
+
+
+            /*
+                Book b = new Book();
+                object obj = b;
+                Console.WriteLine(obj);
+                Console.WriteLine(b.ToString());
+                Console.WriteLine(b.Equals(b));
+                Console.WriteLine(b.GetHashCode());
+                Console.WriteLine(b.GetType());
+                */
+            #endregion
+
+        }
+    }
+}
